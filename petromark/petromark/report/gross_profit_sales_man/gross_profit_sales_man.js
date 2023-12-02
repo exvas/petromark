6caa8f3987282df981dd650bec81c2f8dbc95890
bibinvabaraham <bibincv237@gmail.com>
2023-12-02 14:30:19 +0530

    feat:Gross profit sales man

---
+++ petromark/petromark/report/gross_profit_sales_man/gross_profit_sales_man.js
@@ -0,0 +1,64 @@
+// Copyright (c) 2023, sammish and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Gross Profit Sales Man"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+		},
+		{
+			"fieldname":"customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer"
+		},
+		{
+			"fieldname":"sales_invoice",
+			"label": __("Sales Invoice"),
+			"fieldtype": "Link",
+			"options": "Sales Invoice"
+		},
+		{
+			"fieldname":"delivery_note",
+			"label": __("Delivery Note"),
+			"fieldtype": "Link",
+			"options": "Delivery Note"
+		},
+		{
+			"fieldname":"update_stock",
+			"label": __("Update Stock"),
+			"fieldtype": "Check",
+			"default": 1
+		},
+		{
+			"fieldname":"sales_person",
+			"label": __("Sales Person"),
+			"fieldtype": "Link",
+			"options": "Sales Person"
+		},
+	],
+	"formatter": function(value, row, column, data, default_formatter) {
+
+
+		value = default_formatter(value, row, column, data);
+
+		if (data.bold) {
+
+			value = $(`<span>${value}</span>`);
+			var $value = $(value).css("font-weight", "bold");
+			value = $value.wrap("<p></p>").parent().html();
+		}
+
+		return value;
+	},
+};
